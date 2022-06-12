e80794a7f2b641b52577801bc084c79dc3413c22
Raymond Truong <raymond@truo.ng>
2022-06-11 23:56:12 -0700

    2022 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,13 @@ if ('addEventListener' in window) {
     document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
 }
 
-// pad the bottom of the page since the native Instagram app webview's overlay would cut off the footer
-if (navigator.userAgent.match(/instagram/i)) {
-    document.getElementById("wrapper").style.padding = "0 0 40% 0";
-}
-
 var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 85,
+    typeSpeed: 65,
     loop: false,
 });
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-
 gtag('config', 'UA-123135662-1');
